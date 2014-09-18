c82c84e39c2497d0fee2800481d6d1351a39fcbd
Moiz K. Malik <moiz@heuretix.com>
2014-09-17 21:49:01 -0400

    Initial Commit

---
+++ CrystalBall/THAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THAppDelegate.h
+//  CrystalBall
+//
+//  Created by Moiz K. Malik on 9/17/14.
+//  Copyright (c) 2014 Moiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrystalBall/THViewController.h
@@ -0,0 +1,13 @@
+//
+//  THViewController.h
+//  CrystalBall
+//
+//  Created by Moiz K. Malik on 9/17/14.
+//  Copyright (c) 2014 Moiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THViewController : UIViewController
+
+@end
